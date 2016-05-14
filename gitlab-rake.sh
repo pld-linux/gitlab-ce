97728af534932985afcf160daa2c7d1541a9cdf7
Elan Ruusamäe <glen@delfi.ee>
2016-05-15 01:28:26 +0300

    add gitlab-rake.sh
    this would behave like gitlab-rake from omnibus install

---
+++ gitlab-rake.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+
+# Unset ENV variables that might interfere with
+# omnibus-gitlab ruby env (looking at you rvm)
+for ruby_env_var in RUBYOPT \
+                    BUNDLE_BIN_PATH \
+                    BUNDLE_GEMFILE \
+                    GEM_PATH \
+                    GEM_HOME
+do
+	unset $ruby_env_var
+done
+
+cd /var/lib/gitlab
+exec sudo -u gitlab bundle exec rake RAILS_ENV=production "$@"
